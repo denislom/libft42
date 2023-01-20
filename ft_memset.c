384523be7ebaf07658f3697dc719f1a337e0d11a
Denis Lom <dlom@c3r8s1.42prague.com>
2023-01-20 18:11:53 +0100

    Makefile, ft_memset.c and libft.h edited

--- ft_memset.c
+++ ft_memset.c
@@ -9,21 +9,8 @@
 /*   Updated: 2023/01/15 20:31:28 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
-
-// void	*ft_memset(void *str, int c, size_t n)
-// {
-// 	unsigned char	*ptr;
-
-// 	ptr = (unsigned char*)str;
-// 	while (n > 0)
-// 	{
-// 		*ptr = (unsigned char) c;
-// 		ptr++;
-// 		n--;
-// 	}
-// 	return (str);
-// }
 
 void	*ft_memset(void *s, int c, size_t n)
 {
--- libft.h
+++ libft.h
@@ -1,3 +1,28 @@
-#include <stdio.h>
-#include <stddef.h>
-#include <stdint.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlom <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 15:24:58 by dlom              #+#    #+#             */
+/*   Updated: 2023/01/19 15:25:02 by dlom             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+int		ft_isalnum(int a);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char	*str);
+void	*ft_memset(void *s, int c, size_t n);
+
+#endif
