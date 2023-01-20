7e9c3085bb4f232c1017d1e0b52145425a13e751
Denis Lom <lom.denis@seznam.cz>
2023-01-20 23:06:09 +0100

    ft_strlcpy.c modified, needed to change

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -12,7 +12,36 @@
 
 #include "libft.h"
 
-size_t	strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	
+	size_t	i;
+
+	i = 0;
+	if(size != 0)
+	{
+		while ((src[i] != '\0') && (i < size - 1))
+		{
+			dest [i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (ft_strlen(dest));
 }
+
+// size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	if (size != 0)
+// 	{
+// 		while (src[i] != '\0' && i < size - 1)
+// 		{
+// 			dst[i] = src[i];
+// 			i++;
+// 		}
+// 		dst[i] = '\0';
+// 	}
+// 	return (ft_strlen(src));
+// }
