4d5839fe55643219cd77d12b9d9cadd56fa05b14
Denis Lom <dlom@c3r8s1.42prague.com>
2023-01-21 17:08:46 +0100

    ft_bzero.c, ft_strlcat.c, libft.h changed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 14:13:44 by dlom              #+#    #+#             */
-/*   Updated: 2023/01/21 15:56:46 by dlom             ###   ########.fr       */
+/*   Updated: 2023/01/21 17:04:48 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dstlen;
 	size_t	srclen;
-	size_t	remain;
 	size_t	i;
 	size_t	j;
 
 	dstlen = ft_strlen(dst);
 	srclen = ft_strlen(src);
-	remain = dstlen - size;
-	if (remain == 0)
-		return (dstlen + srclen);
+	if (size <= dstlen)
+		return (srclen + size);
 	i = dstlen;
 	j = 0;
-	while ((src[j]) && (j < remain))
+	while ((src[j]) && (j < size - dstlen - 1))
 		dst[i++] = src[j++];
 	dst[i] = '\0';
 	return (dstlen + srclen);
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:24:58 by dlom              #+#    #+#             */
-/*   Updated: 2023/01/21 15:56:31 by dlom             ###   ########.fr       */
+/*   Updated: 2023/01/21 17:07:18 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_strlen(const char	*str);
-size_t	strlcat(char *dest, const char *src, size_t size);
 void	*ft_memset(void *s, int c, size_t n);
-void	bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 
 #endif
