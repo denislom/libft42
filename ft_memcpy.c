3b268c7e31231cfb304008b93b87be5ac265d0ce
Denis Lom <dlom@c2r4s3.42prague.com>
2023-01-23 16:42:01 +0100

    ft_memcpy.c changed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,24 +6,44 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 09:42:12 by dlom              #+#    #+#             */
-/*   Updated: 2023/01/21 10:29:56 by dlom             ###   ########.fr       */
+/*   Updated: 2023/01/23 16:40:47 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// void	*ft_memcpy(void *dest, const void *src, size_t n)
+// {
+// 	size_t		i;
+// 	char		*t_dest;
+// 	char		*t_src;
+
+// 	if (!dest && !src)
+// 		return (NULL);
+// 	t_dest = (char *)dest;
+// 	t_src = (char *)src;
+// 	while (i < n)
+// 	{
+// 		t_dest[i] = t_src[i];
+// 		i++;
+// 	}
+// 	return ((void *)t_dest);
+// }
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int			i;
-	char		*t_dest;
-	const char	*t_src;
+	size_t		i;
+	char		*cdest;
+	char		*csrc;
 
-	t_dest = dest;
-	t_src = src;
-	while (n--)
+	if (!dest && !src)
+		return (NULL);
+	cdest = (char *)dest;
+	csrc = (char *)src;
+	i = 0;
+	while (i < n)
 	{
-		t_dest[i] = t_src[i];
+		cdest[i] = csrc[i];
 		i++;
 	}
-	return (dest);
+	return ((void *)cdest);
 }
