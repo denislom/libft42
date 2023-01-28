ed019d8e0bd76fc2ab19f9c7da17a7001f722584
Denis Lom <dlom@c3r8s1.42prague.com>
2023-01-28 09:49:23 +0100

    ft_strnstr.c and libft.h changed

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,12 +14,14 @@
 //the functions return a pointer to the first occurence of the substring
 //returns null if a match is not found
 //the function iterates over all possible up to the specified maximum length
-char	*strnstr(const char *str, const char *substr, size_t len)
+char	*ft_strnstr(const char *str, const char *substr, size_t len)
 {
 	size_t	substr_len;
 
 	substr_len = ft_strlen(substr);
-	if (!*substr)
+	if (!*substr && len == 0)
+		return (0);
+	if (substr[0] == '\0')
 		return ((char *)str);
 	while (len > substr_len)
 	{
