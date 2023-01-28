66b51e41843b267a963128d1c4ab77806e80eeeb
Denis Lom <dlom@c3r8s1.42prague.com>
2023-01-28 09:33:34 +0100

    ft_memcmp.c changed

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -12,7 +12,9 @@
 
 #include "libft.h"
 
-/*returns a negative vallue if the first vlock is
+/*
+Compares a specified number of bztes of two memory areas
+returns a negative vallue if the first vlock is
 lexicographically less than second
 returns 0 if two memory blocks are equal
 returns a positive value if the first memory 
@@ -26,11 +28,9 @@ int	ft_memcmp(const void *ptr1, const void *ptr2, size_t n)
 	p1 = (unsigned char *)ptr1;
 	p2 = (unsigned char *)ptr2;
 	i = 0;
-	while (n--)
-	{
-		if (p1[i] != p2[i])
-			return (*p1 - *p2);
+	if (n <= 0)
+		return (0);
+	while ((p1[i] == p2[i]) && (i < n -1))
 		i++;
-	}
-	return (0);
+	return (p1[i] - p2[i]);
 }
