82b793f25375fa82282eb93b02e50c27434817ab
Denis Lom <dlom@c3r8s1.42prague.com>
2023-02-04 10:46:27 +0100

    ft_striteri.c norminette corrections

--- ft_striteri.c
+++ ft_striteri.c
@@ -11,8 +11,8 @@
 /* ************************************************************************** */
 
 #include "libft.h"
- /*
- DESCRIPTION: Applies the function ’f’ on each character of
+/*
+DESCRIPTION: Applies the function ’f’ on each character of
 the string passed as argument, passing its index
 as first argument. Each character is passed by
 address to ’f’ to be modified if necessary.
@@ -22,10 +22,10 @@ RETURNs None
 PARAMETRES: s: The string on which to iterate.
 f: The function to apply to each character.
  */
-void ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
-	
+
 	i = 0;
 	while (s[i++])
 		f(i, &s[i]);
