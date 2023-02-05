ad5df4c636d4f43d411ad3090dd72090a553a978
Denis Lom <dlom@c3r8s1.42prague.com>
2023-02-05 14:38:00 +0100

    ft_lstdelone.c added, Makefile and libft.h changed

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ SRCS		=	ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c ft_isascii.c 
 				ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
 OBJS		=	${SRCS:.c=.o}
 BONUS		=	ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c\
-				
+				ft_lstdelone.c
 OBJSB		=	$(BONUS:.c=.o)
 NAME		=	libft.a
 INCS		=	libft.h
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:24:58 by dlom              #+#    #+#             */
-/*   Updated: 2023/02/05 14:31:29 by dlom             ###   ########.fr       */
+/*   Updated: 2023/02/05 14:37:18 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
